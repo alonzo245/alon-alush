8028c0ec89c29a1f22e872d8cee424bdb9966cbf
Alon Alush <alon.alush.mail@gmail.com>
2024-04-18 19:19:38 +0300

    save

--- src/data/user.js
+++ src/data/user.js
@@ -6,7 +6,7 @@ import githubProjects from "./githubProjects";
 
 export default {
     name: "Alon Alush",
-    currentJobTitle: "Full-Stack Softwer Engineer",
+    currentJobTitle: "Full-Stack Engineer",
     linkedin: "https://il.linkedin.com/in/alonalush",
     github: "https://github.com/alonzo245",
     resumeTitle: "My Resume",
