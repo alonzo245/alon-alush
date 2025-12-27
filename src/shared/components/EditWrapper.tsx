83a8167a1bc9f78cd57a1aee08016cbc02019612
Alon Alush <alon.alush.mail@gmail.com>
2025-12-28 00:40:54 +0200

    tailwind

--- src/shared/components/EditWrapper.tsx
+++ src/shared/components/EditWrapper.tsx
@@ -1,7 +1,6 @@
 import { AiFillEdit } from "react-icons/ai";
 
 import React, { ReactNode } from "react";
-import styled from "@emotion/styled";
 import { useGlobalState } from "../hooks/useGlobalState";
 
 interface EditWrapperProps {
@@ -12,12 +11,8 @@ export default function EditWrapper({ children }: EditWrapperProps): React.JSX.E
     const { state } = useGlobalState();
 
     return (
-        <Container>
+        <div className="relative">
             {state?.edit && <AiFillEdit size={20} color={"#fff"} />} {children}
-        </Container>
+        </div>
     );
 }
-
-const Container = styled.div`
-    position: relative;
-`;
--- src/shared/components/FeatureCarousel.tsx
+++ src/shared/components/FeatureCarousel.tsx
@@ -5,134 +5,94 @@ import { Carousel } from "react-responsive-carousel";
 // import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./carousel.min.css";
 import useScreenSize from "../hooks/useScreenSize";
-import { mobileThreshold, DESKTOP_MQ, WIDE_MQ } from "../../constants/config";
+import { mobileThreshold } from "../../constants/config";
 import workFeatures from "../../assets/workFeatures";
-import styled from "@emotion/styled";
 
 export default function FeatureCarousel(): React.JSX.Element {
     const { width } = useScreenSize();
 
     const { state } = useGlobalState();
     return (
-        <StyledCarousel
-            labels={undefined}
-            showStatus={false}
-            showThumbs={true}
-            showIndicators={false}
-            showArrows={false}
-            // thumbWidth={"40px"}
-            renderThumbs={() => {
-                if ((state?.modalData?.thumbnails?.length || 0) <= 1) return null;
-                return (state?.modalData?.thumbnails || []).map((item: any, i: number) => {
-                    if (item?.type === "youtube") {
-                        return (
-                            <Thumb
-                                alt=""
-                                key={i}
-                                src={workFeatures[item?.name as keyof typeof workFeatures]}
-                            />
-                        );
-                    }
-                    return (
-                        <div key={i}>
-                            {item?.type === "wide" ? (
-                                <Thumb
+        <div className="w-full self-center flex-[2] desktop:w-1/2 desktop:flex-1 wide:w-1/2 wide:flex-1 [&_.carousel-root]:!h-full">
+            <Carousel
+                labels={undefined}
+                showStatus={false}
+                showThumbs={true}
+                showIndicators={false}
+                showArrows={false}
+                // thumbWidth={"40px"}
+                renderThumbs={() => {
+                    if ((state?.modalData?.thumbnails?.length || 0) <= 1) return null;
+                    return (state?.modalData?.thumbnails || []).map((item: any, i: number) => {
+                        if (item?.type === "youtube") {
+                            return (
+                                <img
                                     alt=""
                                     key={i}
                                     src={workFeatures[item?.name as keyof typeof workFeatures]}
-                                    style={{ height: "60px" }}
+                                    className="h-full w-auto"
+                                />
+                            );
+                        }
+                        return (
+                            <div key={i}>
+                                {item?.type === "wide" ? (
+                                    <img
+                                        alt=""
+                                        key={i}
+                                        src={workFeatures[item?.name as keyof typeof workFeatures]}
+                                        className="h-full w-auto"
+                                        style={{ height: "60px" }}
+                                    />
+                                ) : (
+                                    <img
+                                        alt=""
+                                        key={i}
+                                        src={workFeatures[item as keyof typeof workFeatures]}
+                                        className="h-full w-auto"
+                                    />
+                                )}
+                            </div>
+                        );
+                    });
+                }}
+            >
+                {(state?.modalData?.items || []).map((item: any, i: number) => {
+                    return (
+                        <div
+                            key={i}
+                            style={{ height: width > mobileThreshold ? "620px" : "180px" }}
+                        >
+                            {item?.type === "youtube" ? (
+                                <iframe
+                                    width="100%"
+                                    height={width > mobileThreshold ? "620px" : "180px"}
+                                    src={item?.url}
+                                    frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen
+                                    title="Embedded youtube"
                                 />
                             ) : (
-                                <Thumb
-                                    alt=""
-                                    key={i}
-                                    src={workFeatures[item as keyof typeof workFeatures]}
-                                />
+                                <div className="overflow-hidden h-full flex items-center justify-center desktop:mb-0 wide:mb-0">
+                                    <img
+                                        alt=""
+                                        key={i}
+                                        src={
+                                            item?.name
+                                                ? workFeatures[
+                                                      item?.name as keyof typeof workFeatures
+                                                  ]
+                                                : workFeatures[item as keyof typeof workFeatures]
+                                        }
+                                        className="!w-auto h-full"
+                                    />
+                                </div>
                             )}
                         </div>
                     );
-                });
-            }}
-        >
-            {(state?.modalData?.items || []).map((item: any, i: number) => {
-                return (
-                    <div key={i} style={{ height: width > mobileThreshold ? "620px" : "180px" }}>
-                        {item?.type === "youtube" ? (
-                            <iframe
-                                width="100%"
-                                height={width > mobileThreshold ? "620px" : "180px"}
-                                src={item?.url}
-                                frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen
-                                title="Embedded youtube"
-                            />
-                        ) : (
-                            <ImgWrapper>
-                                <Img
-                                    alt=""
-                                    key={i}
-                                    src={
-                                        item?.name
-                                            ? workFeatures[item?.name as keyof typeof workFeatures]
-                                            : workFeatures[item as keyof typeof workFeatures]
-                                    }
-                                />
-                            </ImgWrapper>
-                        )}
-                    </div>
-                );
-            })}
-        </StyledCarousel>
+                })}
+            </Carousel>
+        </div>
     );
 }
-
-const StyledCarousel = styled(Carousel)`
-    width: 100%;
-    align-self: center;
-    flex: 2;
-    .carousel-root {
-        height: 100% !important;
-    }
-
-    ${DESKTOP_MQ} {
-        width: 50%;
-        flex: 1;
-    }
-
-    ${WIDE_MQ} {
-        width: 50%;
-        flex: 1;
-    }
-`;
-
-const Thumb = styled.img`
-    height: 100%;
-    width: auto;
-    //transform: scale(1.2);
-`;
-
-const ImgWrapper = styled.div`
-    overflow: hidden;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    //background-color: red;
-
-    ${DESKTOP_MQ} {
-        margin-bottom: 0px;
-    }
-
-    ${WIDE_MQ} {
-        margin-bottom: 0px;
-    }
-`;
-
-const Img = styled.img`
-    width: auto !important;
-
-    height: 100%;
-    //height: auto;
-    //height: 200px;
-`;
