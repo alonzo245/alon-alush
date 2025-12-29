ac57ed3b474883a2be4596533aa6a50b2b91f1f0
Alon Alush <alon.alush.mail@gmail.com>
2025-12-29 14:12:45 +0200

    save

--- src/features/home/Avatar.tsx
+++ src/features/home/Avatar.tsx
@@ -5,13 +5,7 @@ const Avatar = memo((): React.JSX.Element => {
     return (
         <div className="relative">
             <div className="relative rounded-[250px] h-[250px] w-[250px] overflow-hidden desktop:rounded-[300px] desktop:h-[350px] desktop:w-[350px] wide:rounded-[300px] wide:h-[550px] wide:w-[550px]">
-                <img
-                    src={Alon}
-                    alt="Alon Alush - Profile picture"
-                    loading="lazy"
-                    width={550}
-                    height={550}
-                />
+                <img src={Alon} alt="Alon Alush" loading="lazy" width={550} height={550} />
             </div>
         </div>
     );
